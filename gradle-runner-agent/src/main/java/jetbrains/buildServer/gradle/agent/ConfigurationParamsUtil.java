8bc71e563b1d082fd1dc185d0df3041ff0c32474
Pavel.Sher <Pavel.Sher@jetbrains.com>
2011-07-13 12:38:50 +0000

    TW-4313: fix tests, simplify code

--- gradle-runner-agent/src/main/java/jetbrains/buildServer/gradle/agent/ConfigurationParamsUtil.java
+++ gradle-runner-agent/src/main/java/jetbrains/buildServer/gradle/agent/ConfigurationParamsUtil.java
@@ -24,26 +24,28 @@ import jetbrains.buildServer.runner.JavaRunnerConstants;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
+import static jetbrains.buildServer.util.StringUtil.emptyIfNull;
+
 public class ConfigurationParamsUtil
 {
   public static @NotNull String getGradleHome(Map<String, String> runParameters)
   {
-    return preventNull(runParameters.get(GradleRunnerConstants.GRADLE_HOME), "");
+    return emptyIfNull(runParameters.get(GradleRunnerConstants.GRADLE_HOME));
   }
 
   public static @NotNull String getJavaArgs(Map<String, String> runParameters)
   {
-    return StringUtil.newLineToSpaceDelimited(preventNull(runParameters.get(JavaRunnerConstants.JVM_ARGS_KEY), ""));
+    return StringUtil.newLineToSpaceDelimited(emptyIfNull(runParameters.get(JavaRunnerConstants.JVM_ARGS_KEY)));
   }
 
   public static @NotNull String getGradleTasks(Map<String, String> runParameters)
   {
-    return preventNull(runParameters.get(GradleRunnerConstants.GRADLE_TASKS), "");
+    return emptyIfNull(runParameters.get(GradleRunnerConstants.GRADLE_TASKS));
   }
 
   public static @NotNull List<String> getGradleParams(Map<String, String> runParameters)
   {
-    return CommandLineArgumentsUtil.extractArguments(runParameters.get(GradleRunnerConstants.GRADLE_PARAMS));
+    return CommandLineArgumentsUtil.extractArguments(emptyIfNull(runParameters.get(GradleRunnerConstants.GRADLE_PARAMS)));
   }
 
   public static boolean isParameterEnabled(final Map<String, String> runParameters, final String key)
@@ -53,15 +55,10 @@ public class ConfigurationParamsUtil
 
   public static @NotNull String getGradleInitScript(Map<String, String> runParameters)
   {
-    return preventNull(runParameters.get(GradleRunnerConstants.GRADLE_INIT_SCRIPT), "");
-  }
-
-  private static @NotNull <T> T preventNull(T t, T defaultValue)
-  {
-    return t == null ? defaultValue : t;
+    return emptyIfNull(runParameters.get(GradleRunnerConstants.GRADLE_INIT_SCRIPT));
   }
 
   public static String getGradleWPath(final Map<String, String> runnerParameters) {
-    return preventNull(runnerParameters.get(GradleRunnerConstants.GRADLE_WRAPPER_PATH), "");
+    return emptyIfNull(runnerParameters.get(GradleRunnerConstants.GRADLE_WRAPPER_PATH));
   }
 }
