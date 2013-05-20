c72de3c7f3f1e456d6c490be5e1a6ea0a7942ae6
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2013-05-20 10:45:29 +0000

    fix blinking tests. Old snapshots of Gradle 1.1 were removed

--- gradle-runner-agent/src/test/java/jetbrains/buildServer/gradle/test/integration/GradleVersionErrorsListenerTest.java
+++ gradle-runner-agent/src/test/java/jetbrains/buildServer/gradle/test/integration/GradleVersionErrorsListenerTest.java
@@ -3,7 +3,6 @@ package jetbrains.buildServer.gradle.test.integration;
 import java.io.File;
 import java.io.IOException;
 import jetbrains.buildServer.agent.AgentRuntimeProperties;
-import jetbrains.buildServer.gradle.GradleRunnerConstants;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.testng.annotations.Test;
@@ -18,7 +17,6 @@ public class GradleVersionErrorsListenerTest extends GradleRunnerServiceMessageT
   public void testGradleVersionErrorInTestOutput(final String gradleVersion) throws Exception {
     myBuildEnvVars.put(AgentRuntimeProperties.AGENT_BUILD_PARAMS_FILE_ENV,
                          new File(myProjectRoot, "src/test/resources/testProjects/buildNumber.properties").getAbsolutePath());
-    myRunnerParams.put(GradleRunnerConstants.GRADLE_WRAPPER_FLAG, Boolean.TRUE.toString());
     final GradleRunConfiguration gradleRunConfiguration = new GradleRunConfiguration("wrappedProjectD", "-i --continue clean test",
                                                                                      "test_error_output_Sequence.txt");
     gradleRunConfiguration.setGradleVersion(gradleVersion);
