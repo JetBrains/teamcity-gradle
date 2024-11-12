e7f3751ed9d339b0372cec99a66bcd85f69bbf79
Semyon Naumov <semen.naumov@jetbrains.com>
2024-11-12 18:41:15 +0000

    TW-90316: Adapt to changes in RunnerDependencyCacheSettingsProvider
    Merge-request: TC-MR-12697
    Merged-by: Semyon Naumov <Semen.Naumov@jetbrains.com>

--- gradle-runner-agent/src/main/kotlin/jetbrains/buildServer/gradle/depcache/GradleDependencyCacheSettingsProvider.kt
+++ gradle-runner-agent/src/main/kotlin/jetbrains/buildServer/gradle/depcache/GradleDependencyCacheSettingsProvider.kt
@@ -1,9 +1,11 @@
 package jetbrains.buildServer.gradle.depcache
 
 import jetbrains.buildServer.agent.AgentLifeCycleListener
+import jetbrains.buildServer.agent.AgentRunningBuild
+import jetbrains.buildServer.agent.BuildFinishedStatus
 import jetbrains.buildServer.agent.cache.depcache.DependencyCacheProvider
 import jetbrains.buildServer.agent.cache.depcache.DependencyCacheSettingsProviderRegistry
-import jetbrains.buildServer.agent.cache.depcache.buildFeature.RunnerDependencyCacheSettingsProvider
+import jetbrains.buildServer.agent.cache.depcache.buildFeature.BuildRunnerDependencyCacheSettingsProvider
 import jetbrains.buildServer.agent.cache.depcache.cacheroot.CacheRootPublishPaths
 import jetbrains.buildServer.agent.cache.depcache.cacheroot.CacheRootPublisher
 import jetbrains.buildServer.gradle.GradleRunnerConstants
@@ -13,7 +15,7 @@ class GradleDependencyCacheSettingsProvider(
     private val eventDispatcher: EventDispatcher<AgentLifeCycleListener>,
     private val cacheSettingsProviderRegistry: DependencyCacheSettingsProviderRegistry,
     private val cacheProvider: DependencyCacheProvider
-) : RunnerDependencyCacheSettingsProvider<GradleDependenciesChangedInvalidator>(
+) : BuildRunnerDependencyCacheSettingsProvider(
     eventDispatcher, cacheSettingsProviderRegistry, cacheProvider,
     GradleRunnerConstants.RUNNER_TYPE,
     GradleRunnerConstants.DISPLAY_NAME,
@@ -21,9 +23,12 @@ class GradleDependencyCacheSettingsProvider(
     GradleDependencyCacheConstants.FEATURE_TOGGLE_GRADLE_DEPENDENCY_CACHE,
     GradleDependencyCacheConstants.FEATURE_TOGGLE_GRADLE_DEPENDENCY_CACHE_DEFAULT
 ) {
+    var postBuildInvalidator: GradleDependenciesChangedInvalidator? = null
+        private set
 
-    protected override fun createPostBuildInvalidator(): GradleDependenciesChangedInvalidator {
-        return GradleDependenciesChangedInvalidator()
+    protected override fun createPostBuildInvalidators(): List<GradleDependenciesChangedInvalidator> {
+        postBuildInvalidator = GradleDependenciesChangedInvalidator()
+        return listOf(postBuildInvalidator!!)
     }
 
     protected override fun createCacheRootPublisher(): CacheRootPublisher {
@@ -34,5 +39,9 @@ class GradleDependencyCacheSettingsProvider(
                 listOf("**/*.lock", "**/gc.properties")
             )
         })
+    }
+
+    public override fun buildFinished(build: AgentRunningBuild, buildStatus: BuildFinishedStatus) {
+        postBuildInvalidator = null
     }
 }
