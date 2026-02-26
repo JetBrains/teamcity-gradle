605d4b7cfac642fc923cfeac9f3dc5208d547424
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-02-26 10:38:47 +0100

    fixup! Support Gradle configuration cache by disabling reading of the properties file

--- gradle-runner-agent/src/main/java/jetbrains/buildServer/gradle/agent/propertySplit/InitScriptParametersConstants.java
+++ gradle-runner-agent/src/main/java/jetbrains/buildServer/gradle/agent/propertySplit/InitScriptParametersConstants.java
@@ -10,29 +10,28 @@ public enum InitScriptParametersConstants {
     TEAMCITY_BUILD_GRADLE_TEST_JVM_ARGS_KEY("gradle.test.jvmargs", ""),
     TEAMCITY_BUILD_STACKTRACE_LOG_DIR_KEY("teamcity.build.stacktraceLogDir", ""),
     TEAMCITY_BUILD_CHANGED_FILES_KEY("teamcity.build.changedFiles.file", ""),
-    TEAMCITY_BUILD_TEMP_DIR_KEY( "teamcity.build.tempDir", ""),
+    TEAMCITY_BUILD_TEMP_DIR_KEY("teamcity.build.tempDir", ""),
     TEAMCITY_CONFIGURATION_TEST_NAME_FORMAT_KEY("teamcity.internal.gradle.testNameFormat", ""),
     TEAMCITY_CONFIGURATION_IGNORE_SUITE_FORMAT_KEY("teamcity.internal.gradle.ignoredSuiteFormat", ""),
     TEAMCITY_CONFIGURATION_USE_TEST_RETRY_PLUGIN_KEY("teamcity.internal.gradle.useTestRetryPlugin", "true"),
     TEAMCITY_CONFIGURATION_BUILD_BRANCH_KEY("teamcity.build.branch", "");
 
-
-    private  final String key;
+    private final String key;
     private final String envKey;
     private final String defaultValue;
 
-     InitScriptParametersConstants(String key, String defaultValue) {
+    InitScriptParametersConstants(String key, String defaultValue) {
         this.key = key;
         this.defaultValue = defaultValue;
         this.envKey = key.toUpperCase().replace('.', '_');
     }
 
     public String getKey() {
-         return key;
+        return key;
     }
 
     public String getEnvKey() {
-         return envKey;
+        return envKey;
     }
 
     public String getDefaultValue() {
