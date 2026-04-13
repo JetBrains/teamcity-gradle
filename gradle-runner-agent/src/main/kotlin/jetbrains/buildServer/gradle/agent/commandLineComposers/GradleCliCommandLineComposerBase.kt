5adbbed7d124fa73da8c963fd6e6ed6fc4addc37
Boris Yakhno <boris.yakhno@jetbrains.com>
2026-04-13 11:28:13 +0200

    TW-100409 Use system properties instead of build properties to configure the init_v2.gradle and init_since_8.gradle init scripts
    This makes both scripts uniformly independent from the build properties file and streamlines the changes introduced in TW-99117 for init_since_8.gradle.

--- gradle-runner-agent/src/main/kotlin/jetbrains/buildServer/gradle/agent/commandLineComposers/GradleCliCommandLineComposerBase.kt
+++ gradle-runner-agent/src/main/kotlin/jetbrains/buildServer/gradle/agent/commandLineComposers/GradleCliCommandLineComposerBase.kt
@@ -2,6 +2,7 @@ package jetbrains.buildServer.gradle.agent.commandLineComposers
 
 import jetbrains.buildServer.agent.runner.ProgramCommandLine
 import jetbrains.buildServer.agent.runner.SimpleProgramCommandLine
+import jetbrains.buildServer.gradle.agent.propertySplit.InitScriptParametersConstants
 import jetbrains.buildServer.gradle.agent.tasks.GradleTasksComposer
 
 abstract class GradleCliCommandLineComposerBase(private val tasksComposer: GradleTasksComposer) : GradleCommandLineComposer {
@@ -16,6 +17,7 @@ abstract class GradleCliCommandLineComposerBase(private val tasksComposer: Gradl
                     parameters.pluginsDir.toFile()
                 )
             )
+            addAll(getSystemPropertiesForInitScript(parameters).map { it.getArgumentFromSystemProperty() })
             addAll(parameters.gradleTasks)
         }
 
@@ -26,4 +28,30 @@ abstract class GradleCliCommandLineComposerBase(private val tasksComposer: Gradl
             gradleParameters
         )
     }
+
+    private fun getSystemPropertiesForInitScript(parameters: GradleCommandLineComposerParameters): Sequence<Pair<String, String>> = sequence {
+        listOf(
+            InitScriptParametersConstants.TEAMCITY_BUILD_GRADLE_TEST_JVM_ARGS_KEY,
+            InitScriptParametersConstants.TEAMCITY_BUILD_STACKTRACE_LOG_DIR_KEY,
+            InitScriptParametersConstants.TEAMCITY_BUILD_CHANGED_FILES_KEY,
+            InitScriptParametersConstants.TEAMCITY_BUILD_TEMP_DIR_KEY,
+        ).forEach { propertyName ->
+            parameters.runnerContext.buildParameters.systemProperties[propertyName]?.let { value ->
+                yield(propertyName to value)
+            }
+        }
+
+        listOf(
+            InitScriptParametersConstants.GRADLE_RUNNER_DO_NOT_POPULATE_GRADLE_PROPERTIES,
+            InitScriptParametersConstants.TEAMCITY_CONFIGURATION_USE_TEST_RETRY_PLUGIN_KEY,
+            InitScriptParametersConstants.TEAMCITY_CONFIGURATION_TEST_NAME_FORMAT_KEY,
+            InitScriptParametersConstants.TEAMCITY_CONFIGURATION_IGNORE_SUITE_FORMAT_KEY,
+        ).forEach { parameterName ->
+            parameters.configParameters[parameterName]?.let { value ->
+                yield(parameterName to value)
+            }
+        }
+    }
+
+    private fun Pair<String, String>.getArgumentFromSystemProperty() = "-D${first}=${second}"
 }
